49a769eefea78b60b7dc32af6c4d3a0f2e59a8a2
Jian Leong <jianwei1801@gmail.com>
2013-07-27 19:51:54 -0700

    personalized information

--- personalized_information.py
+++ personalized_information.py
@@ -1,29 +1,32 @@
-def get_airport_data(device_id = '12345'):
+def get_username(device_id):
+    return "Richie Zeng"
+
+def get_airport_data(device_id):
     #returns a dictionary
     return {"op":"+",
-             "type": "flight_info",
+            "type": "flight_info",
             "user_name": "Richie Zeng",
-            "profile_image_url": "google.com", 
-            "flight_number": "UA 456", 
+            "profile_image_url": "google.com",
+            "flight_number": "UA 456",
             "flight_gate": "G45",
-            "flight_time": "2045", 
+            "flight_time": "2045",
             "destination": "Miami, FL"}
 
 
 def get_destination_info():
     #returns an array of places of interest, hotels, cab companies
-    return('Golden Gate Park', 'The Hilton', 'Hertz Car Rental')
+    return("Golden Gate Park", "The Hilton", "Hertz Car Rental")
 
 def get_food_info():
     #returns dictionary of restaurants and food rating
-    return{'McDonalds':3.4,
-           'TGIF':4.7,
-           'Crepes A Go Go':5.0}
+    return{"McDonalds":3.4,
+           "TGIF":4.7,
+           "Crepes A Go Go":5.0}
 
 def get_movie_info():
     #returns dictionary of movies and personalized ratings
-    return{'Despicable Me 2':5.0, 'Lone Ranger':2.7, 'Monsters University':4.5}
+    return{"Despicable Me 2":5.0, "Lone Ranger":2.7, "Monsters University":4.5}
 
 def get_store_info():
     #returns dictionary of stores and rating/distance from current location
-    return{'GAP':(4.5, 13), 'Abercrombie&Fitch':(2.1, 2)}
+    return{"GAP":(4.5, 13), "Abercrombie&Fitch":(2.1, 2)}
