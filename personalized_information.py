1824f127922cd43364a038cd12e0dcdb73418e39
Jian Leong <jianwei1801@gmail.com>
2013-07-28 07:41:02 -0700

    major overhaul

--- personalized_information.py
+++ personalized_information.py
@@ -1,63 +1,163 @@
 from yelp import *
+import collections as cl
 
 USER_DICT = {}
 FLIGHT_DICT = {}
+PLACE_DICT = {}
+RENTALCAR_DICT = cl.defaultdict(set)
+RESTAURANT_DICT = {}
 
-def add_user(device_id, display_name, profile_image_url):
-    USER_DICT[device_id] = {}
-    USER_DICT[device_id]["display_name"] = display_name
-    USER_DICT[device_id]["profile_image_url"] = profile_image_url
 
-def add_flight(device_id, flight_number, flight_gate, flight_time, arrival_time, destination):
-    FLIGHT_DICT[device_id] = {}
-    FLIGHT_DICT[device_id]["flight_number"] = flight_number
-    FLIGHT_DICT[device_id]["flight_gate"] = flight_gate
-    FLIGHT_DICT[device_id]["flight_time"] = flight_time
-    FLIGHT_DICT[device_id]["arrival_time"] = arrival_time
-    FLIGHT_DICT[device_id]["destination"] = destination
+class User(object):
 
-def get_username(device_id):
-    return get_displayname(device_id).replace(" ", "_").replace(".", "_").lower()
+    def __init__(self, device_id, display_name, profile_image_url):
+        self.device_id = device_id
+        self.display_name = display_name
+        self.user_name = display_name.replace(" ", "_").replace(".", "_").lower()
+        self.profile_image_url = profile_image_url
+        USER_DICT[device_id] = self
 
-def get_displayname(device_id):
-    return USER_DICT[device_id]["display_name"]
+    @staticmethod
+    def get_display_name(device_id):
+        return USER_DICT[device_id].display_name
 
-def get_airport_data(device_id):
+    @staticmethod
+    def get_user_name(device_id):
+        return USER_DICT[device_id].user_name
+
+    @staticmethod
+    def get_pic_url(device_id):
+        return USER_DICT[device_id].profile_image_url
+
+
+class Flight(object):
+
+    def __init__(self, device_id, flight_number, flight_gate, departure_time, arrival_time, destination):
+        self.device_id = device_id
+        self.flight_number = flight_number
+        self.flight_gate = flight_gate
+        self.departure_time = departure_time
+        self.arrival_time = arrival_time
+        self.destination = destination
+        FLIGHT_DICT[device_id] = self
+
+    @property
+    def dict(self):
+        return {"op":"+",
+                "type": "flight_info",
+                "display_name": User.get_display_name(self.device_id),
+                "user_name": User.get_user_name(self.device_id),
+                "profile_image_url": User.get_pic_url(self.device_id),
+                "flight_number": self.flight_number,
+                "flight_gate": self.flight_gate,
+                "departure_time": self.departure_time,
+                "arrival_time": self.arrival_time,
+                "destination": self.destination}
+
+
+class Place(object):
+
+    def __init__(self, device_id, name, img, distance):
+        self.device_id = device_id
+        self.name = name
+        self.img = img
+        self.distance = distance
+        PLACE_DICT[device_id] = self
+
+    @property
+    def dict(self):
+        return {"op": "+",
+                "type": "places",
+                "user_name": User.get_user_name(self.device_id),
+                "display_name": User.get_display_name(self.device_id),
+                "user_name": User.get_user_name(self.device_id),
+                "profile_image_url": User.get_pic_url(self.device_id),
+                "name": self.name,
+                "img": self.img,
+                "distance": self.distance}
+
+
+class RentalCar(object):
+
+    def __init__(self, device_id, model, price, img, number):
+        self.device_id = device_id
+        self.model = model
+        self.price = price
+        self.img = img
+        self.number = number
+        RENTALCAR_DICT[device_id].add(self)
+
+    @property
+    def dict(self):
+        cars = RENTALCAR_DICT[self.device_id]
+        data = []
+        for car in cars:
+            data.append({"model": self.model,
+                         "price": self.price,
+                         "img": self.img,
+                         "number": self.number})
+        return {"op": "+",
+                "type": "rentals",
+                "user_name": User.get_user_name(self.device_id),
+                "display_name": User.get_display_name(self.device_id),
+                "user_name": User.get_user_name(self.device_id),
+                "profile_image_url": User.get_pic_url(self.device_id),
+                "cars": data}
+
+
+class Restaurant(object):
+
+    def __init__(self, device_id, name, rating, image_url):
+        self.device_id = device_id
+        self.name = name
+        self.rating = rating
+        self.image_url = image_url
+        RESTAURANT_DICT[device_id] = self
+
+    @property
+    def dict(self):
+        return {"op": "+",
+                "type": "restaurants",
+                "user_name": User.get_user_name(self.device_id),
+                "display_name": User.get_display_name(self.device_id),
+                "user_name": User.get_user_name(self.device_id),
+                "profile_image_url": User.get_pic_url(self.device_id),
+                "name": self.name,
+                "img": self.image_url,
+                "distance": self.rating}
+
+def populate_data(data, cards):
+    for card in cards:
+        data.append(card.dict)
+
+def get_data(device_id, place):
     data = []
-    user_name = get_username(device_id)
-    display_name = get_displayname(device_id)
-    pic_url = USER_DICT[device_id]["profile_image_url"]
+    user_name = User.get_user_name(device_id)
+    display_name = User.get_display_name(device_id)
+    pic_url = USER_DICT[device_id].profile_image_url
     flight = FLIGHT_DICT[device_id]
-    return [{"op":"+",
-             "type": "flight_info",
-             "display_name": display_name,
-             "user_name": user_name,
-             "profile_image_url": pic_url,
-             "flight_number": flight["flight_number"],
-             "flight_gate": flight["flight_gate"],
-             "flight_time": flight["flight_time"],
-             "arrival_time": flight["arrival_time"],
-             "destination": flight["destination"]},]
-
-def get_destination_info():
-    #returns an array of places of interest, hotels, cab companies
-    return("Golden Gate Park", "The Hilton", "Hertz Car Rental")
-
-def get_restaurant_data(location, search_term):
-    #returns dictionary of restaurants and food rating
-    return locate_food(location, search_term)
+    if place == "sf":
+        if display_name == "Richie Z.":
+            populate_data(data, sf_rich_data)
+    if place == "miami":
+        if display_name == "Richie Z.":
+            populate_data(data, mia_rich_data)
+        if display_name == "Jian L.":
+            populate_data(data, mia_jw_data)
+    return data
 
 
-def get_movie_info():
-    #returns dictionary of movies and personalized ratings
-    return{"Despicable Me 2":5.0, "Lone Ranger":2.7, "Monsters University":4.5}
+rich = User("518b237bef45d835", "Richie Z.", "https://fbcdn-sphotos-c-a.akamaihd.net/hphotos-ak-ash4/215356_10151641982947594_144941694_n.jpg")
+jian = User("kevid", "Jian L.", "https://lh6.googleusercontent.com/-TpuvTBw3vx8/UQM7IOmkmBI/AAAAAAAAAbI/DzNehhnVEWE/w256-h257-no/375563_10151168765508994_657905193_n.jpeg")
+f1 = Flight("518b237bef45d835", "UA 456", "G45", "11:45 AM", "6:55 PM", "Miami, FL")
+rc1 = RentalCar("518b237bef45d835", "Honda Accord 2012", "$30.09", "../img/accord.jpg", "305-871-0300")
+rc2 = RentalCar("518b237bef45d835", "Toyota Camry 2009", "$24.09", "../img/camry.jpg", "305-871-0300")
+rc3 = RentalCar("518b237bef45d835", "Mitsubishi Evolution X", "$36.09", "../img/evo.jpg", "305-876-1800")
+p1 = Place("518b237bef45d835", "Hollywood Beach", "../img/hollywood-beach.jpg", "21.5 mi")
+r1 = Restaurant("518b237bef45d835", "People's Bar-B-Que", "6.2 mi", "http://s3-media1.ak.yelpcdn.com/bphoto/MyucYR9AxCPKRvXQSZKQNg/l.jpg")
+r2 = Restaurant("kevid", "Wo Bu Che Tau", "4.1 mi", "http://s3-media2.ak.yelpcdn.com/bphoto/N2r0JV3GjbDlt5_HZS3uGQ/l.jpg")
+p2 = Place("kevid", "Wo Ai Ni", "http://www.miasci.org/blog/wp-content/uploads/2010/03/MIASCI-Park-View4.jpg", "9 shingshong")
 
-def get_store_info():
-    #returns dictionary of stores and rating/distance from current location
-    return{"GAP":(4.5, 13), "Abercrombie&Fitch":(2.1, 2)}
-
-
-add_user("1", "Richie Z.", "https://fbcdn-sphotos-c-a.akamaihd.net/hphotos-ak-ash4/215356_10151641982947594_144941694_n.jpg")
-add_flight("1", "UA 456", "G45", "11:45 AM", "6:55 PM", "Miami, FL")
-add_user("kevid", "Jian L.", "https://lh6.googleusercontent.com/-TpuvTBw3vx8/UQM7IOmkmBI/AAAAAAAAAbI/DzNehhnVEWE/w256-h257-no/375563_10151168765508994_657905193_n.jpeg")
-add_flight("kevid", "UA 456", "G45", "11:45 AM", "6:55 PM", "Miami, FL")
+sf_rich_data = (f1, p1)
+mia_rich_data = (rc1, r1)
+mia_jw_data = (p2, r2)
